716e17d37936de52ab40c1846696606a12f9133b
osvaldosanches <vado.benites@gmail.com>
2021-02-04 17:29:40 -0300

    envio para a fila sqs

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParse = require("body-parser");
 const cors = require("cors");
 const app = express();
 const mysql = require("mysql");
+const axios = require('axios');
 
 const db = mysql.createPool({
     host: "localhost",
@@ -19,15 +20,6 @@ const db = mysql.createPool({
     //queryTimeout: 1000000,
 
 });
-
-//app.get('/',(req,res)=>{
-    //const sqlInsert = "INSERT INTO norma (id, nome, descricao, codigo, inicioVigencia, fimVigencia) VALUES ('4', 'Norma ambiental de PLA 010', 'Material PLA para fabricação de etiqueta plástica ou lacre de produto', '004', '01012021', '31122021');";
-    //db.query(sqlInsert,(err,result)=>{
-    //    console.log(err);
-    //    res.send('hello Osvaldo teste2 ');
-    //}); 
-
-//});
 
 app.get("/api/get",(req,res)=>{
 
@@ -63,54 +55,20 @@ app.post("/api/insert",(req,res)=>{
     const nome = req.body.nome
     const descricao = req.body.descricao
 
-    const sqlInsert = "INSERT INTO norma (nome, descricao, codigo, inicioVigencia, fimVigencia) VALUES ( ?, ?, '012', '01022021', '31122021');";
-
-
-    db.query(sqlInsert,[nome, descricao],(err,result)=>{
-        console.log(nome);
-        console.log(descricao);
-        console.log(result);        
-        console.log(err); 
-    }); 
+    axios
+    //.post('http://hml.portalgeprosro.brasilseg.com.br/api/cargas/cargarInserir/risco_emissao', {
+    .get('https://sqs.sa-east-1.amazonaws.com/687255601585/AvancarEtapaProcesso?Action=SendMessage&MessageBody=NOVO2 - 04 de fevereiro 2021', {
+        //todo: 'Buy the milk'
+    })
+    .then(res => {
+        console.log(`statusCode: ${res.statusCode}`)
+        //console.log(res)
+    })
+    .catch(error => {
+        console.error(error)
+    })
 
 });
-
-app.delete("/api/delete/:nome", (req,res) =>{
-    const nome = req.params.nome;
-    const sqlDelete = "delete from norma where nome = ?;";
-
-    db.query(sqlDelete, nome, (err, result)=>{
-
-        console.log("osvaldo!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
-        console.log(nome);
-        console.log(err);
-        console.log(result);
-
-        if(err) console.log(err);
-
-    });
-
-})
-
-app.put("/api/update", (req,res) =>{
-    const nome = req.body.nome;
-    const descricao = req.body.descricao;
-
-    const sqlUpdate = "update norma set descricao = ? where nome = ?;";
-
-    db.query(sqlUpdate, [descricao,nome], (err, result)=>{
-
-        console.log("osvaldo!");
-        console.log(nome);
-        console.log(descricao);
-        console.log(err);
-        console.log(result);
-
-        if(err) console.log(err);
-
-    });
-
-})
 
 app.listen(3001,()=>{
     console.log("running on port 3001");
