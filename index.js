d8ddacaec190a5d22092e3b44dabf5b640baef2d
osvaldosanches <vado.benites@gmail.com>
2021-02-04 23:33:16 -0300

    ajuste do retorno

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const app = express();
 const mysql = require("mysql");
 const axios = require('axios');
-const journal = [];
 
 const db = mysql.createPool({
     host: "localhost",
@@ -17,94 +16,46 @@ const db = mysql.createPool({
 
 app.get("/api/get",(req,res)=>{
 
+    const resposta = [];
+
     axios
     .get('https://sqs.sa-east-1.amazonaws.com/687255601585/AvancarEtapaProcesso?Action=ReceiveMessage', {
     })
-    .then(res => {
+    .then(result => {
 
-        console.log(`statusCode: ${res.status}`)
-        //console.log(res.data.ReceiveMessageResponse.ReceiveMessageResult.messages)
-        //const json = messages;
-        //const obj = JSON.parse(json);
+        console.log(`statusCode: ${result.status}`)
         
-        // console.log(obj.body);
-
-        //console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>osvaldo")
-        //console.log(res.data)
-
-        //res.data.ReceiveMessageResponse.ReceiveMessageResult.messages[0].Body;
-        const messages = res.data.ReceiveMessageResponse.ReceiveMessageResult.messages;
-        //console.log(messages);
+        const messages = result.data.ReceiveMessageResponse.ReceiveMessageResult.messages;
         //console.log(messages[0].Body);
         //console.log(res.data.ReceiveMessageResponse.ReceiveMessageResult.messages[0].Body);
-
+        //console.log(messages[0]);
 
         for (var i = 0; i < messages.length; i++) {
             console.log("for posicao "+i+": "+messages[i].Body);
-            journal.push({nome: "osvaldo", descricao: "sanches2"});
+            resposta.push({nome:+""+messages[i].Body, descricao:+""+messages[i].MD5OfBody}); 
 
         }
-        //journal.push({nome: "osvaldo", descricao: "sanches2"});
 
-        //console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>")
-       // for (var i = 0; i < journal.length; i++) {
-         //   console.log(journal[i]);
-      //  }
-
-        //console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>osvaldo2")
-        //const obj = JSON.parse(res.data);
-        //console.log(obj);
-
-        //var obj = JSON.stringify(messages);
-        //console.log(obj);
-
-        //console.log(">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>osvaldo3")
-        //console.log(res.toString());
-        //res.send(journal);
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        //res.send(res.toString()); //funciona
+        //res.send(res.data); //funciona
+        //res.send(res); //nao 
+        //res.send(res.data.ReceiveMessageResponse.ReceiveMessageResult.messages[0].Body); nao
+    
+        //const retorno = [
+        // {nome: "x", descricao: "y"},
+        // {nome: "x", descricao: "y"},
+        //];
+        //console.log(resposta);
+        res.send(resposta);
 
      })
     .catch(error => {
       console.error(error)
     })
 
-
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    //res.send(res.toString()); //funciona
-    //res.send(res.data); //funciona
-    //res.send(res); //nao 
-    //res.send(res.data.ReceiveMessageResponse.ReceiveMessageResult.messages[0].Body); nao
-
-
-   //const retorno = [
-   // {nome: "osvaldo", descricao: "sanches"},
-   // {nome: "haydee", descricao: "hirai"},
-  //];
-
-       //const messages2 = res.data.ReceiveMessageResponse.ReceiveMessageResult.messages;
-  //console.log(messages);
-  //console.log(messages[0].Body);
-       //console.log(res.data.ReceiveMessageResponse.ReceiveMessageResult.messages[0].Body);
-
-
-        //for (var i = 0; i < messages2.length; i++) {
-           // console.log("posicao nova "+i+": "+messages2[i].Body);
-        // journal.push({nome: "osvaldo", descricao: "sanches2"});
-
-       // }
-
-    //journal.push({nome: "osvaldo", descricao: "sanches2"});
-    //console.log("final tamanho: "+journal.length);
-    //for (var i = 0; i < journal.length; i++) {
-     //   console.log("final for valor "+journal[i]);
-     //}
-
-   res.send(journal);
-
-
 });
-
-
 
 app.use(cors());
 
